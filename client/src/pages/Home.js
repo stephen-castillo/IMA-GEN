84ff67e12ccfa123f02b8b74fa163785bef1f3c8
Addit <adam.garza33@hotmail.com>
2023-07-05 00:32:00 -0500

    made div class container in Home.js

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,18 +1,10 @@
-import { useAuth } from "../util/auth";
+import '../styles/Home.css';
 
 export default function Home() {
-  const { isLoggedIn, user } = useAuth();
   return (
     <div>
-      {/* TODO: display logged in user's username */}
-      <h1>Welcome {isLoggedIn ? user.username : "Guest"}!</h1>
-      <hr />
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo cumque
-        explicabo ipsum, facilis repellendus omnis amet in accusantium quisquam
-        nam qui consectetur sunt distinctio nemo molestiae ratione. Iure,
-        aliquam debitis.
-      </p>
+      <div className='container'></div>
+        
     </div>
   );
 }
