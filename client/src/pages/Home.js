db3e3765838ad66e347910984adaf6a7734abdfc
Addit <adam.garza33@hotmail.com>
2023-07-05 00:34:49 -0500

    style bubbles

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,7 +3,13 @@ import '../styles/Home.css';
 export default function Home() {
   return (
     <div>
-      <div className='container'></div>
+      <div className='container'>
+
+        <div className='bubbles'>
+          <span></span>
+        </div>
+      </div>
+      
         
     </div>
   );
