bec70f5467814efa16dc9142a920576dc250e058
Michael Muniz <m.s.muniz@sbcglobal.net>
2023-07-05 16:22:28 -0500

    still working on return for ProtectedUserPage display. Add FormField component for searching.

--- client/src/pages/ProtectedUserPage.js
+++ client/src/pages/ProtectedUserPage.js
@@ -27,23 +27,35 @@ const renderDate = (date) =>
       fetchPolicy: "network-only",
     });
     console.log(user);
-  
+
     const [loading, setLoading] = useState(false);
     const [allPosts, setAllPosts] = useState(null);
 
     return (
       <section className="max-w-7xl mx-auto">
         <div>
-        <h1>Welcome {user.username}!</h1>
-        <p>
-          Last Login:{" "}
-          {loading
-            ? "Loading..."
-            : data && renderDate(new Date(data.me.lastLogin))}
-        </p>
+          <h1>Welcome {user.username}!</h1>
+          <p>
+            Last Login:{" "}
+            {loading
+              ? "Loading..."
+              : data && renderDate(new Date(data.me.lastLogin))}
+          </p>
+          <h1 className="font-extrabold text-[#1535d4] text-[32px]">
+            AI Image Gallery
+          </h1>
+          <p className="mt-2 text-[#1535d4] text-[16px] max-w-[500px]">
+            DALL-E AI Visual Odyssey: Embark on a visual journey through a
+            captivating series of AI-generated images, crafted by DALL-E's
+            imaginative algorithms.
+          </p>
+        </div>
+
+        <div className="mt-16">
+          <FormField />
         </div>
       </section>
-    )
+    );
   }
   
 
