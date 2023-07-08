2355462131398e234db0a81dbf34cea89402491c
Addit <adam.garza33@hotmail.com>
2023-07-08 15:54:01 -0500

    finished getRandomPrompt function for retrieving random prompts

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -1,5 +1,10 @@
 import { surpriseMePrompts } from '../components'
 
 export function getRandomPrompt (prompt) {
-    
+    const randomIndex = Math.floor(Math.random() *
+    surpriseMePrompts.length);
+    const randomPrompt = surpriseMePrompts[randomIndex];
+
+
+    return randomPrompt;
 }
