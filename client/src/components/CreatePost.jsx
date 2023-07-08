b7ec16734e7ab3a601f096365a4d70384a67ca4d
Addit <adam.garza33@hotmail.com>
2023-07-08 16:18:50 -0500

    contact into API while waiting for image, loader

--- client/src/components/CreatePost.jsx
+++ client/src/components/CreatePost.jsx
@@ -5,12 +5,19 @@ import { getRandomPrompt } from '../util';
 import { FormField, Loader } from '../components'
 
 const CreatePost = () => {
+    // navigate back to homepage once post is created
     const navigate = useNavigate();
+    // useState field
     const [form, setForm] = useState({
         name: '',
         prompt: '',
         photo: '',
     });
+    
+    // contact API while waiting for image
+    const [generatingImg, setGeneratingImg] = useState(false);
+    // Loading
+    const [loading, setLoading] = useState(false);
 
     return (
         <div>CreatePost</div>
