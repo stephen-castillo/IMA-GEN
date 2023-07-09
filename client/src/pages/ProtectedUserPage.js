4c2e83ba9280d67e62e7eacd1a37c01618c1082d
Michael Muniz <m.s.muniz@sbcglobal.net>
2023-07-08 21:06:10 -0500

    Added the handleSearchChange function to reduce the number of search operations after the user stops typing by 500 milliseconds after the user stops typing.

--- client/src/pages/ProtectedUserPage.js
+++ client/src/pages/ProtectedUserPage.js
@@ -8,22 +8,19 @@
 
 // export default ProtectedUserPage
 
-
 import { useQuery } from "@apollo/client";
 import { useAuth } from "../util/auth";
 import { ME } from "../util/queries";
 
-import React, { useEffect, useState } from 'react';
-import { Card, FormField, Loader } from '../components';
+import React, { useEffect, useState } from "react";
+import { Card, FormField, Loader } from "../components";
 
-  const renderDate = (date) =>
+const renderDate = (date) =>
   `${date.toLocaleDateString()} ${date.toLocaleTimeString()}`;
 
 const RenderCards = ({ data, title }) => {
   if (data?.length > 0) {
-    return (
-      data.map((post) => <Card key={post._id} {...post} />)
-    );
+    return data.map((post) => <Card key={post._id} {...post} />);
   }
 
   return (
@@ -31,77 +28,88 @@ const RenderCards = ({ data, title }) => {
   );
 };
 
+export default function ProtectedUserPage() {
+  const { user } = useAuth();
+  const { data, loading } = useQuery(ME, {
+    fetchPolicy: "network-only",
+  });
+  console.log(user);
+
+  const [loadingb, setLoading] = useState(false);
+  const [allPosts, setAllPosts] = useState(null);
+  const [searchText, setSearchText] = useState("");
+  const [searchTimeout, setSearchTimeout] = useState(null);
+  const [searchedResults, setSearchedResults] = useState(null);
 
 
-  export default function ProtectedUserPage() {
-    const { user } = useAuth();
-    const { data, loading } = useQuery(ME, {
-      fetchPolicy: "network-only",
-    });
-    console.log(user);
 
-    const [loadingb, setLoading] = useState(false);
-    const [allPosts, setAllPosts] = useState(null);
-    const [searchText, setSearchText] = useState('');
+  const handleSearchChange = (e) => {
+    clearTimeout(searchTimeout);
+    setSearchText(e.target.value);
 
-    return (
-      <section className="max-w-7xl mx-auto">
-        <div>
-          <h1 className="mt-8 ml-4 text-[white] text-[18px] max-w-[200px]">
-            Welcome {user.username}!
-          </h1>
-          <p className="mt-2 ml-4 text-[#666f75] text-[10px] max-w-[500px]">
-            Last Login:{" "}
-            {loading
-              ? "Loading..."
-              : data && renderDate(new Date(data.me.lastLogin))}
-          </p>
-          <h1 className="mt-6 ml-40 font-extrabold text-[#a8eb12] text-[40px] max-w-[600px]">
-            IMA-GEN
-          </h1>
-          <p className="mt-2 text-[#666f75] text-[14px] max-w-[600px]">
-            A DALL-E AI Visual Odyssey: Embark on a visual journey through a
-            captivating series of AI-generated images, crafted by DALL-E's
-            imaginative algorithms.
-          </p>
-        </div>
-
-        <div className="mt-16">
-          <FormField
-            labelName="Search posts"
-            type="text"
-            name="text"
-            placeholder="Search something..."
-            value={searchText}
-            // handleChange={handleSearchChange}
-          />
-        </div>
-        <div className="mt-10">
-          {loadingb ? (
-            <div className="flex justify-center items-center">
-              <Loader />
-            </div>
-          ) : (
-            <>
-              {searchText && (
-                <h2 className="font-medium text-[#1535d4] text-xl mt-5 mb-3">
-                  Showing Resuls for{" "}
-                  <span className="text-[#1535d4]">{searchText}</span>:
-                </h2>
-              )}
-              <div className="grid lg:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-3">
-                {searchText ? (
-                  <RenderCards data={[]} title="No Search Results Found" />
-                ) : (
-                  <RenderCards data={[]} title="No Posted AI Images Yet" />
-                )}
-              </div>
-            </>
-          )}
-        </div>
-      </section>
+    setSearchTimeout(
+      setTimeout(() => {
+        const searchResult = allPosts.filter((item) => item.name.toLowerCase().includes(searchText.toLowerCase()) || item.prompt.toLowerCase().includes(searchText.toLowerCase()));
+        setSearchedResults(searchResult);
+      }, 500),
     );
-  }
-  
+  };
 
+  return (
+    <section className="max-w-7xl mx-auto">
+      <div>
+        <h1 className="mt-8 ml-4 text-[white] text-[18px] max-w-[200px]">
+          Welcome {user.username}!
+        </h1>
+        <p className="mt-2 ml-4 text-[#666f75] text-[10px] max-w-[500px]">
+          Last Login:{" "}
+          {loading
+            ? "Loading..."
+            : data && renderDate(new Date(data.me.lastLogin))}
+        </p>
+        <h1 className="mt-6 ml-40 font-extrabold text-[#a8eb12] text-[40px] max-w-[600px]">
+          IMA-GEN
+        </h1>
+        <p className="mt-2 text-[#666f75] text-[14px] max-w-[600px]">
+          A DALL-E AI Visual Odyssey: Embark on a visual journey through a
+          captivating series of AI-generated images, crafted by DALL-E's
+          imaginative algorithms.
+        </p>
+      </div>
 
+      <div className="mt-16">
+        <FormField
+          labelName="Search posts"
+          type="text"
+          name="text"
+          placeholder="Search something..."
+          value={searchText}
+          handleChange={handleSearchChange}
+        />
+      </div>
+      <div className="mt-10">
+        {loadingb ? (
+          <div className="flex justify-center items-center">
+            <Loader />
+          </div>
+        ) : (
+          <>
+            {searchText && (
+              <h2 className="font-medium text-[#1535d4] text-xl mt-5 mb-3">
+                Showing Resuls for{" "}
+                <span className="text-[#1535d4]">{searchText}</span>:
+              </h2>
+            )}
+            <div className="grid lg:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-3">
+              {searchText ? (
+                <RenderCards data={[]} title="No Search Results Found" />
+              ) : (
+                <RenderCards data={[]} title="No Posted AI Images Yet" />
+              )}
+            </div>
+          </>
+        )}
+      </div>
+    </section>
+  );
+}
