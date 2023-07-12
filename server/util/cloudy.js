4a573df3e1487d0e7e61f710122c27e37ff3c925
stephen-castillo <122939431+stephen-castillo@users.noreply.github.com>
2023-07-12 04:55:57 -0500

    Condensed redundant api file and fixed 3rd party api code

--- server/util/cloudy.js
+++ server/util/cloudy.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-import {v2 as cloudinary} from 'cloudinary';
+const { v2: cloudinary } = require('cloudinary');
 
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
