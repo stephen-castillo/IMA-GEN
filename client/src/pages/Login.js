7a5b77c35da91f9d7b6ee95c77f14401a8ad83c6
Addit <adam.garza33@hotmail.com>
2023-07-12 16:29:57 -0500

    finished styling of Login form

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -105,36 +105,36 @@ import { useAuth } from "../util/auth";
 import '../styles/Login.css';
 import logo from '../assets/IMAGEN.png'
 
-// export default function Login() {
-//   const { isLoggedIn, login, loading, error } = useAuth();
-//   const [formState, setFormState] = useState({
-//     email: "",
-//     password: "",
-//   });
-//   const location = useLocation();
+export default function Login() {
+  const { isLoggedIn, login, loading, error } = useAuth();
+  const [formState, setFormState] = useState({
+    email: "",
+    password: "",
+  });
+  const location = useLocation();
 
-//   useEffect(() => {
-//     if (error) {
-//       // TODO: replace window alert with custom alert
-//       alert(error);
-//     }
-//   }, [error]);
+  useEffect(() => {
+    if (error) {
+      // TODO: replace window alert with custom alert
+      alert(error);
+    }
+  }, [error]);
 
-//   const handleInputChange = (evt) => {
-//     const { name, value } = evt.target;
-//     setFormState((prevState) => ({ ...prevState, [name]: value }));
-//   };
+  const handleInputChange = (evt) => {
+    const { name, value } = evt.target;
+    setFormState((prevState) => ({ ...prevState, [name]: value }));
+  };
 
-//   const handleSubmit = async (evt) => {
-//     evt.preventDefault();
-//     login(formState);
-//   };
+  const handleSubmit = async (evt) => {
+    evt.preventDefault();
+    login(formState);
+  };
 
-//   if (isLoggedIn) {
-//     // navigate to page user was redirected from or the home page.
-//     const from = location.state?.from?.pathname || "/protectedCluster";
-//     return <Navigate to={from} replace />;
-//   }
+  if (isLoggedIn) {
+    // navigate to page user was redirected from or the home page.
+    const from = location.state?.from?.pathname || "/protectedCluster";
+    return <Navigate to={from} replace />;
+  }
 
 //   return (
 //     <div className="max-w-xlg mx-auto">
@@ -186,7 +186,7 @@ import logo from '../assets/IMAGEN.png'
 // }
 
 
-export default function Login() {
+
   return (
     <div className='grid grid-cols-1 sm:grid-cols-2 h-screen w-full'>
       <div className='hidden sm:block'>
@@ -195,27 +195,33 @@ export default function Login() {
 
       <div className='bg-[#0c192c]-800 flex flex-col justify-center'>
         <form className='max-w-[400px] w-full mx-auto bg-gray-800 p-8 px-8 rounded-lg'>
-          <h2 className='text-4x1 dark:text-white font-extrabold text-center'>Log In</h2>
-          <div className='flex flex-col text-gray-400 py-2'>
+          <h2 className='text-4x1 text-white font-extrabold text-center'>Log In</h2>
+          <div className='flex flex-col text-white-500 py-2'>
             <label>Email</label>
             <input className='rounded-lg bg-gray-700 mt-2 p-2 focus:border-blue-500 focus:ng-gray-800 fcous:outline-none'
-            type='email'/>
+            disabled={loading}
+            id='email'
+            type='email'
+            name='email'
+            placeholder='Enter Email'
+            value={formState.email}
+            onChange={handleInputChange}/>
           </div>
           {/* <div className='flex flex-col text-gray-400 py-2'>
             <label>Username</label>
             <input className='rounded-lg bg-gray-700 mt-2 p-2 focus:border-blue-500 focus:ng-gray-800 fcous:outline-none'
             type='text'/>
           </div> */}
-          <div className='flex flex-col text-gray-400 py-2'>
+          <div className='flex flex-col text-white-500 py-2'>
             <label>Password</label>
             <input className='rounded-lg bg-gray-700 mt-2 p-2 focus:border-blue-500 focus:ng-gray-800 fcous:outline-none'
             type='password'/>
           </div>
-          <div className='flexbox jusitfy-between text-gray-400 py-2 '>
+          <div className='flexbox jusitfy-between text-white-500 py-2 '>
             <p className='flex items-center'><input className='mr-2' type='checkbox'/> Remember Me</p>
             <p>Forgot Password</p>
           </div>
-          <button className='w-full my-5 py-2 bg-red-500'>Sign In</button>
+          <button className='w-full my-5 py-2 bg-red-500 shadow-lg shadow-red-500/50 hover:shadow-red-500/40 text-white font-semibold rounded-lg'>Log In</button>
         </form>
       </div>
 
