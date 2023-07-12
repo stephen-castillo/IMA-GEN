967cea748bd69cb7ebe7bb4b56cca64602d93709
Michael Muniz <m.s.muniz@sbcglobal.net>
2023-07-12 02:22:14 -0500

    Fixed color contrast with background color to se button better.

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -134,7 +134,7 @@ const CreatePost = () => {
                         type='button'
                         /* onClick={generateImage} */
                         onClick={generateImage}
-                        className='text-white bg-[#ff1867]-700 font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center'
+                        className='text-white bg-[#4A648C] font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center'
                         >
                            {generatingImg ? 'Generating...' : 'Generate'} 
                     </button>
